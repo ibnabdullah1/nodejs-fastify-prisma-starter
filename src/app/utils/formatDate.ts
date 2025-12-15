40a935c444b555eb587b9fe109edaa763755f61b
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:29:00 +0600

    --no-verify

---
+++ src/app/utils/formatDate.ts
@@ -0,0 +1,19 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  };
+
+  const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(date);
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "PM" : "AM";
+  const formattedTime = `${hours % 12 || 12}:${minutes
+    .toString()
+    .padStart(2, "0")} ${ampm}`;
+
+  return `${formattedDate} ${formattedTime}`;
+};
