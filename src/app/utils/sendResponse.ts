e5a54d6ef7aab2113f4791cf1832881ec9f9a66a
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:28:56 +0600

    --no-verify

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+
+type TResponse<T> = {
+  status: number;
+  success: boolean;
+  message?: string;
+  data?: T;
+};
+
+const sendResponse = <T>(res: Response, data: TResponse<T>) => {
+  res.status(data?.status).json({
+    success: data.success,
+    status: data?.status,
+    message: data.message,
+    data: data.data,
+  });
+};
+
+export default sendResponse;
