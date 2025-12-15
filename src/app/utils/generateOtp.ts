2e5f6a6735a7fbe597ebc27ad039da32a5170dba
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:51:50 +0600

    refactor: update comments (commit 25038)

--- src/app/utils/generateOtp.ts
+++ src/app/utils/generateOtp.ts
@@ -1,14 +1,2 @@
 export const generateOtp = (): string =>
    Math.floor(1000 + Math.random() * 9000).toString();
-
-// Commit 19
-
-// Commit 22
-
-// Commit 42
-
-// Commit 72
-
-// Commit 108
-
-// Commit 120
