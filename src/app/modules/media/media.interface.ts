467dac44edf80b3f8fe1705f10ca2705b002a477
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:28:53 +0600

    --no-verify

---
+++ src/app/modules/media/media.interface.ts
@@ -0,0 +1,12 @@
+export interface IMedia {
+  name: string;
+  url: string;
+  folder?: string;
+  type: string;
+  size: number;
+  uploadedBy?: number | null;
+  altText?: string;
+  title?: string;
+  caption?: string;
+  description?: string;
+}
