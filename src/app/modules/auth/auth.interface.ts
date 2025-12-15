18d5c90a1ed2c7ed99c4dfc05710306de851fb42
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:28:56 +0600

    --no-verify

---
+++ src/app/modules/auth/auth.interface.ts
@@ -0,0 +1,24 @@
+import { UserRole } from "../user/user.interface";
+
+export interface IAuth {
+  email: string;
+  password: string;
+  clientInfo: {
+    device: string; // Device type
+    browser: string; // Browser name
+    ipAddress: string; // User IP address
+    pcName?: string; // Optional PC name
+    os?: string; // Optional OS name (Windows, MacOS, etc.)
+    userAgent?: string; // Optional user agent string
+  };
+}
+
+export interface IJwtPayload {
+  user_id: string;
+  name: string;
+  email: string;
+  role: UserRole;
+  is_active: boolean;
+  last_login: Date;
+  profile_photo?: string;
+}
