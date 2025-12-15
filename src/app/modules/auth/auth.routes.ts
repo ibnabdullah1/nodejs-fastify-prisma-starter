8ebaf0ec526dd0218de83bd1141a1fef05e5d55e
ibnabdullah1 <arfathossen541@gmail.com>
2025-12-15 10:28:56 +0600

    --no-verify

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import auth from "../../middleware/auth";
+import clientInfoParser from "../../middleware/clientInfoParser";
+import { UserRole } from "../user/user.interface";
+import { AuthController } from "./auth.controller";
+
+const router = Router();
+
+router.post("/login", clientInfoParser, AuthController.loginUser);
+
+router.post("/refresh-token", AuthController.refreshToken);
+
+router.post(
+  "/change-password",
+  auth(UserRole.SUPER_ADMIN, UserRole.USER),
+  AuthController.changePassword
+);
+
+export const AuthRoutes = router;
